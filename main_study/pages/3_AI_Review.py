6850eaeb8ba617ef264954cd2bf20f3d7d1d0cf6
Laura Spillner <laura.spillner@uni-bremen.de>
2023-04-06 14:58:31 +0200

    changed explanation layout, added bootstrap

--- main_study/pages/3_AI_Review.py
+++ main_study/pages/3_AI_Review.py
@@ -78,21 +78,18 @@ final_user_predictions = {}
 
 for s in songs:
 
-	col1, col2 = st.columns([1, 2])
+	col1, col2, col3 = st.columns([2, 5, 3])
 
 	song = df.loc[s]
 	lyrics = song["lyrics"].replace("\n", "   \n") 
 
 	with col1:
 
+		st.write("Your first prediction was:", user_predictions[s])
+
 		choice = st.radio("Winner or loser?", ["WINNER", "LOSER"], key = str(s), index = 0 if user_predictions[s] == "WINNER" else 1)
 
 		final_user_predictions[s] = choice
-
-		ai_choice = ai_prediction[s]
-		ai_explanation = df.loc[s, "explanation_correct"] if isinstance(df.loc[s, "explanation_correct"], str) else df.loc[s, "explanation_incorrect"]
-		st.markdown(f'AI prediction: {ai_choice}')
-		st.markdown(f'*Explanation*:   \n {ai_explanation}')
 
 		#if choice == "WINNER":
 		#	st.success("You predict: Winner!")
@@ -102,17 +99,46 @@ for s in songs:
 	with col2:
 
 		if choice == "LOSER":
-			st.markdown(f'**{song["song"]}**   \n by *{song["performer"]}* from {song["to_country"]}')
-			with st.expander(lyrics.split("\n")[0] + "[...]"):
-				st.markdown(f'{lyrics}')
+			st.error(f'**{song["song"]}**   \n by *{song["performer"]}* from {song["to_country"]}')
+			#with st.expander(lyrics.split("\n")[0] + "[...]"):
+				#st.error(f'{lyrics}')
+			components.html(
+				#f"""
+				#<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+				#<div class="overflow-auto">{lyrics}</div>	
+				#<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+				#<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+				#<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>				
+				#"""
+	f"""
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div class="overflow-auto">{lyrics}</div>
+    """
+				)
 		elif choice == "WINNER":
 			st.success(f'**{song["song"]}**   \n by *{song["performer"]}* from {song["to_country"]}')
 			with st.expander(lyrics.split("\n")[0] + "[...]"):
 				st.success(f'{lyrics}')
+
 		#elif choice == "LOSER":
 		#	st.error(f'**{song["song"]}**   \n by *{song["performer"]}* from {song["to_country"]}')
 		#	with st.expander(lyrics.split("\n")[0] + "[...]"):
 		#		st.error(f'{lyrics}')
+
+	with col3:
+
+		st.write("This is where the audio goes.")
+		components.html(
+			"""
+			<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6bk11TH82XrT5QUC1gDw9A?utm_source=generator" width="250" height="150" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>"""
+			)
+
+	ai_choice = ai_prediction[s]
+	ai_explanation = df.loc[s, "explanation_correct"] if isinstance(df.loc[s, "explanation_correct"], str) else df.loc[s, "explanation_incorrect"]
+	st.markdown(f'AI prediction: {ai_choice}')
+	st.markdown(f'*Explanation*:   \n {ai_explanation}')
 
 	st.markdown("---")
 
